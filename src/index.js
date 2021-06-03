35168dc490773374e23cc7859daf5ab9281c763b
Victoria <victoriathorsenn@gmail.com>
2021-06-03 01:11:08 -0400

    commit tree ced43fed9f1951d9da8b2eba8312822493445fac

--- src/index.js
+++ src/index.js
@@ -1,15 +1,75 @@
 let addToy = false;
+let ogDiv = document.querySelector('#toy-collection')
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+  
+  
+  
+  const getToys = () => {
+    return fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(toys => {toys.forEach(toy => {
+      createToyData(toy)
+    })
+  })
+}
+
+const createToyData = (toy) => {
+  let divCard = document.createElement('div')
+  divCard.id = 'toyCard'
+  
+  let name = document.createElement('h2')
+  name.innerText = toy.name
+  
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  
+  let likes = document.createElement('p')
+  likes.innerText = toy.likes
+  
+  let button = document.createElement('button')
+  button.id= 'likesBtn'
+  
+  divCard.append(name, img, likes, button)
+  ogDiv.append(divCard)
+}
+
+const createToys = (toy) => {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify ({
+      "name": toy.name.value,
+      "image": toy.image.value,
+      "likes": 0
+    })
+  })
+  .then(resp => resp.json())
+  .then(function(newToy) {
+    createToyData(newToy)
+  })
+}
+
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
+addBtn.addEventListener("click", () => {
+  // hide & seek with the form
+  addToy = !addToy;
+  if (addToy) {
+    toyFormContainer.style.display = "block";
+    toyFormContainer.addEventListener('submit', e => {
+      e.preventDefault()
+      createToys(e.target)
+    })
+  } else {
+    toyFormContainer.style.display = "none";
+  }
 });
+
+getToys()
+
+});
+    
